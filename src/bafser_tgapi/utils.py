c54eb29415a7ff2a6fd835f97230165ebaffdfa2
Mike <63655075+MixelTe@users.noreply.github.com>
2025-10-02 21:19:17 +0300

    v1.4.2

--- src/bafser_tgapi/utils.py
+++ src/bafser_tgapi/utils.py
@@ -160,8 +160,8 @@ def call(method: str, data: JsonObj | dict[str, Any] | None = None, timeout: int
         raise Exception("tgapi call error")
 
 
-def call_async(fn: Callable[[], Any], args: Iterable[Any] = ()):
-    threading.Thread(target=fn, args=args).start()
+def call_async(fn: Callable[[], Any], *args: Any, **kwargs: Any):
+    threading.Thread(target=fn, args=args, kwargs=kwargs).start()
 
 
 def __item_to_json__(item: Any) -> Any:
